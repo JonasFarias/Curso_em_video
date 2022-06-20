3da334d7219c9c458be95f69a7247709dac03abd
Jonas Farias <jhonas.jr@hotmail.com>
2022-06-19 21:27:55 -0300

    Renomeando Exercicios

--- Mundo 2/Exercicios part 2/Exercicio 12 - Validação de Dados.py
+++ Mundo 2/Exercicios part 2/Exercicio 12 - Validação de Dados.py
@@ -1,4 +1,6 @@
-# Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+# Faça um programa que leia o sexo de uma pessoa,
+# mas só aceite os valores 'M' ou 'F'.
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 
 sexo = str(input('Digite seu sexo [M/F]:')).strip().upper()[0]
--- Mundo 2/Exercicios part 2/Exercicio 16 - Progressão Aritmetica V20.py
+++ Mundo 2/Exercicios part 2/Exercicio 16 - Progressão Aritmetica V20.py
@@ -1,4 +1,6 @@
-'''Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura  while.'''
+'''Refaça o DESAFIO 051, lendo o primeiro termo e
+a razão de uma PA, mostrando os 10 primeiros termos da
+progressão usando a estrutura  while.'''
 
 
 print('===' * 10)

592f3cb32275d4cfe63d1f072688338034cb7d31
Jonas Farias <jhonas.jr@hotmail.com>
2022-06-19 21:12:17 -0300

    Create Exercicio 25 - Sequencia de Fibonacci V01.py

---
+++ Mundo 2/Exercicios part 2/Exercicio 25 - Sequencia de Fibonacci V01.py
@@ -0,0 +1,13 @@
+'''Escreva um programa que leia um número N
+inteiro qualquer e mostre na tela os N
+primeiros elementos de uma Sequência de Fibonacci. Exemplo:
+
+0 – 1 – 1 – 2 – 3 – 5 – 8'''
+
+
+print('SEQUENCIA DE FIBONACCI V01')
+print('#' * 30)
+
+sequencia = int(input('Quantos termos você quer mostrar? '))
+contador = 0
+while contador

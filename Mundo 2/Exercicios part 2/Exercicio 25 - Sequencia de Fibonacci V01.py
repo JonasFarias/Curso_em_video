edca3dabab771bfa061d2234021035ab48bf493a
Jonas Farias <jhonas.jr@hotmail.com>
2022-06-20 21:29:22 -0300

    Update Exercicio 25 - Sequencia de Fibonacci V01.py

--- Mundo 2/Exercicios part 2/Exercicio 25 - Sequencia de Fibonacci V01.py
+++ Mundo 2/Exercicios part 2/Exercicio 25 - Sequencia de Fibonacci V01.py
@@ -4,10 +4,20 @@ primeiros elementos de uma Sequência de Fibonacci. Exemplo:
 
 0 – 1 – 1 – 2 – 3 – 5 – 8'''
 
-
+print('#' * 30)
 print('SEQUENCIA DE FIBONACCI V01')
 print('#' * 30)
-
 sequencia = int(input('Quantos termos você quer mostrar? '))
-contador = 0
-while contador
+t1 = 0
+t2 = 1
+
+print('~'*30)
+print('{} - {}'.format(t1, t2), end='')
+contador = 3
+while contador <= sequencia:
+    t3 = t1 + t2
+    print(' - {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    contador += 1
+print(' - FIM ')

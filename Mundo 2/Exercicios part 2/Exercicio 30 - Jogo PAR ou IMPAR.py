7c6f27e676676346c6c38dfda69905b60d675004
Jonas Farias <jhonas.jr@hotmail.com>
2022-06-27 21:07:28 -0300

    Update Exercicio 30 - Jogo PAR ou IMPAR.py

--- Mundo 2/Exercicios part 2/Exercicio 30 - Jogo PAR ou IMPAR.py
+++ Mundo 2/Exercicios part 2/Exercicio 30 - Jogo PAR ou IMPAR.py
@@ -6,10 +6,10 @@ consecutivas que ele conquistou no final do jogo.'''
 from random import randint
 
 print('=-=' * 10)
-print('     PAR ou IMPAR')
+print('    VAMOS JOGAR PAR ou IMPAR')
 print('=-=' * 10)
 
-
+vitoria = 0
 
 while True:
     jogador = int(input('Digite um numero: '))
@@ -18,6 +18,20 @@ while True:
     esolha = ' '
     while esolha not in 'PpIi':
         esolha = str(input('Escolha Par ou Impar: ')).strip().upper()[0]
-    print(f'Você Jogou {jogador}, e o computador jogou {computador}, o total é de {total}')
-
+    print(f'Você Jogou {jogador}, e o computador jogou {computador}, o total é de {total}', end=' ')
+    print('Deu PAR ' if total % 2 == 0 else 'Deu IMPAR')
+    if esolha == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU! ')
+            vitoria += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif esolha == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU! ')
+            vitoria += 1
+        else:
+            print('Você PERDEU! ')
+            break
 

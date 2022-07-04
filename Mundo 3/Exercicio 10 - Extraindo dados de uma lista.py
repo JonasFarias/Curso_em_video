fa50ca0de48873c27b693aa17463fbb7d0c32ca3
Jonas Farias <jhonas.jr@hotmail.com>
2022-07-04 17:52:53 -0300

    Update Exercicio 10 - Extraindo dados de uma lista.py

--- Mundo 3/Exercicio 10 - Extraindo dados de uma lista.py
+++ Mundo 3/Exercicio 10 - Extraindo dados de uma lista.py
@@ -11,10 +11,11 @@ while True:
     numero = int(input('Digite um valor: '))
     numeros.append(numero)
     resposta = str(input('Deseja continuar [S/N]: ')).strip().upper()[0]
-    contador += 1
-    reverso = numeros[:]
     if resposta in 'N':
         break
-print(f'Foram digitados {contador} numeros e os valores digitados foram {numeros}')
-print(f'Os Numeros em ordem Decrescente é: ')
+print(f'Foram digitados {len(numeros)} numeros e os valores digitados foram {numeros}')
+numeros.sort(reverse=True)
+print(f'Os Numeros em ordem Decrescente são: {numeros}')
+numeros.sort()
+print(f'Os numeros em ordem Crescente são: {numeros}')
 

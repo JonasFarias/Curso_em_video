7f865d807b6b74734e64ef863a7e89ec343e42c1
Jonas Farias <jhonas.jr@hotmail.com>
2022-07-20 20:17:13 -0300

    Update Exercicio 12 - Lista Composta e Analise de dados.py

--- Mundo 3/Exercicio 12 - Lista Composta e Analise de dados.py
+++ Mundo 3/Exercicio 12 - Lista Composta e Analise de dados.py
@@ -4,17 +4,13 @@ A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.'''
 
-pessoa = list()
+temporario = []
+principal = []
 
 while True:
-    nome = str(input('Nome: '))
-    peso = float(input('Peso: '))
-    continuar = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
-    if continuar in 'S':
-        pessoa.append([nome, peso])
-    elif continuar in 'N':
-        pessoa.append([nome, peso])
+    temporario.append(str(input('Nome: ')))
+    temporario.append(float(input('Peso: ')))
+    principal.append(temporario[:])
+    resposta = str(input('Quer continuar [S/N]: ')).strip().upper()[0]
+    if resposta in 'N':
         break
-
-
-print(pessoa)
